4e82f54e913a1c26e73f9f1bfe1966544101ab58
kimanicode <karaba.dev@gmail.com>
2025-04-13 20:59:41 +0300

    added video

--- app/components/VideoIntro.jsx
+++ app/components/VideoIntro.jsx
@@ -12,7 +12,7 @@ const VideoIntro = () => {
         <div className="relative pb-[56.25%] h-0 overflow-hidden rounded-2xl shadow-lg">
           <iframe
             className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/YOUR_VIDEO_ID"
+            src="https://www.youtube.com/embed/R5Tl9wYkZl0"
             title="Yvonne Kabui - Introduction"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
